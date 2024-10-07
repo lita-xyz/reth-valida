08130d3418ac27c488c6e6baa18875d9e8f65c29
Lewis Revill <lewis1revill@gmail.com>
2024-10-07 17:00:19 +0100

    TEMP: Minimize for debugging of deserialization

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 
 use reth_primitives::B256;
 use revm::InMemoryDB;
-use reth_valida::primitives::db::InMemoryDBHelper;
-use reth_valida::primitives::mpt::keccak;
-use reth_valida::primitives::processor::EvmProcessor;
 use reth_valida::primitives::ValidaRethInput;
 
 entrypoint::entrypoint!(main);
@@ -12,20 +9,4 @@ entrypoint::entrypoint!(main);
 pub fn main() {
     let mut input = entrypoint::io::read::<ValidaRethInput>().unwrap();
 
-    // Initialize the database.
-    let db = InMemoryDB::initialize(&mut input).unwrap();
-
-    // Execute the block.
-    let mut executor = EvmProcessor::<InMemoryDB> {
-        input,
-        db: Some(db),
-        header: None,
-    };
-    executor.initialize();
-    executor.execute();
-    executor.finalize();
-
-    // Print the resulting block hash.
-    let hash = B256::from(keccak(alloy_rlp::encode(executor.header.unwrap())));
-    entrypoint::io::write(&hash).unwrap();
 }
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,10 +1,5 @@
 pub mod alloy2reth;
-pub mod db;
-pub mod mpt;
-pub mod processor;
 
-use crate::primitives::mpt::MptNode;
-use crate::primitives::mpt::StorageEntry;
 
 use reth_primitives::{Address, Bytes, Header, TransactionSignedNoHash, Withdrawal, B256};
 use revm::primitives::HashMap;
@@ -13,9 +8,6 @@ use serde::{Deserialize, Serialize};
 /// Necessary information to prove the execution of Ethereum blocks inside Valida.
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ValidaRethInput {
-    /// The Keccak 256-bit hash of the parent block's header, in its entirety.
-    pub parent_header: Header,
-
     /// The 160-bit address to which all fees collected from the successful mining of this block
     /// be transferred.
     pub beneficiary: Address,
@@ -30,25 +22,4 @@ pub struct ValidaRethInput {
     /// fewer.
     pub extra_data: Bytes,
 
-    /// A 256-bit hash which, combined with the nonce, proves that a sufficient amount of
-    /// computation has been carried out on this block.
-    pub mix_hash: B256,
-
-    /// The state trie of the parent block.
-    pub parent_state_trie: MptNode,
-
-    /// The storage of the parent block.
-    pub parent_storage: HashMap<Address, StorageEntry>,
-
-    /// The relevant contracts for the block.
-    pub contracts: Vec<Bytes>,
-
-    /// The ancestor headers of the parent block.
-    pub ancestor_headers: Vec<Header>,
-
-    /// A list of transactions to process.
-    pub transactions: Vec<TransactionSignedNoHash>,
-
-    /// A list of withdrawals to process.
-    pub withdrawals: Vec<Withdrawal>,
 }
