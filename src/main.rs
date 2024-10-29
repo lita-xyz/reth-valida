7737ae1e642cd8821f37c403d2ebdd0e3a982a62
Marty Stumpf <thealmartyblog@gmail.com>
2024-10-29 11:54:43 -0700

    Update package name of `valida-rs`. (#12)
    * Update package name.
    
    * Update cargo lock and fn calls.
    
    ---------
    
    Co-authored-by: thealmarty <“thealmartyblog@gmail.com”>

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use reth_valida::primitives::mpt::keccak;
 use reth_valida::primitives::processor::EvmProcessor;
 use reth_valida::primitives::ValidaRethInput;
 
-entrypoint::entrypoint!(main);
+valida_rs::entrypoint!(main);
 
 pub fn main() {
-    let mut input = match entrypoint::io::read_and_deserialize::<ValidaRethInput>() {
+    let mut input = match valida_rs::io::read_and_deserialize::<ValidaRethInput>() {
         Ok(val) => val,
         Err(e) => {
-            entrypoint::io::println(&format!("Error reading/deserializing input: {}", e));
+            valida_rs::io::println(&format!("Error reading/deserializing input: {}", e));
             return;
         }
     };
@@ -21,7 +21,7 @@ pub fn main() {
     let db = match InMemoryDB::initialize(&mut input) {
         Ok(val) => val,
         Err(e) => {
-            entrypoint::io::println(&format!("Error initializing database: {}", e));
+            valida_rs::io::println(&format!("Error initializing database: {}", e));
             return;
         }
     };
@@ -40,12 +40,12 @@ pub fn main() {
     let header = match executor.header {
         Some(val) => val,
         None => {
-            entrypoint::io::println("Error: executor header is None");
+            valida_rs::io::println("Error: executor header is None");
             return;
         }
     };
     let hash = B256::from(keccak(alloy_rlp::encode(header)));
-    if let Err(e) = entrypoint::io::write(&hash) {
-        entrypoint::io::println(&format!("Error writing hash: {}", e));
+    if let Err(e) = valida_rs::io::write(&hash) {
+        valida_rs::io::println(&format!("Error writing hash: {}", e));
     }
 }
